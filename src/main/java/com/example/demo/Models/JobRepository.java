cc030a08fde00b3c69b36aaf002a834b71acf82f
Daylin Henry <djhenry94@gmail.com>
2017-07-07 17:57:49 -0400

    hopefeully done4

--- src/main/java/com/example/demo/Models/JobRepository.java
+++ src/main/java/com/example/demo/Models/JobRepository.java
@@ -8,18 +8,19 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by daylinhenry on 7/5/17.
  */
 public interface JobRepository extends CrudRepository<Job, Integer> {
-/*
+
 
    @Transactional
    @Modifying
    @Query(value = "SELECT job.employer FROM Job, Skills WHERE Job.Skill LIKE CONCAT('%', :skill,'%')", nativeQuery = true)
-   List <String> findBySkillContaining(String skill);
-*/
+   List<String> findBySkillContaining(String skill);
+
 
 
 
